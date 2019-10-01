a1599ca39f46a52c624b58376940b057d1a1caac
Chris Grice <chris@chrisgrice.com>
2019-10-02 00:52:50 +0100

    Tidying up and styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import todoBg from './todopaper.png'
 
 import domtoimage from 'dom-to-image'
 import { saveAs } from 'file-saver'
+import { Helmet } from 'react-helmet'
 
 
 function App() {
@@ -35,8 +36,8 @@ function App() {
         console.log(image.width, image.height)
         console.log(w, h)
 
-        if(w < 700) {
-          alert('Please select an image which is at least 700px wide')
+        if(w < 300) {
+          alert('Please select an image which is at least 300px wide')
         } else {
           setImage(reader.result)
           setImageSize({
@@ -56,28 +57,30 @@ function App() {
 
   const download = async () => {
     const blob = await domtoimage.toBlob(outputRef.current)
-    saveAs(blob, 'to-doose-output.png')
+    saveAs(blob, `to-doose-output-${Date.now()}.png`)
   }
 
   return (
     <div className="App">
+      <Helmet>
+        <title>to-doose</title>
+        <meta name="description" content="It is a lovely day on the internet, and you are a terrible goose." />
+      </Helmet> 
       <h1>to-doose</h1>
       <div className="form">
         <div className="row">
-          <span className="action">1. Choose an image</span>
           <span className="action">
-            <input type="file" name="image" onChange={handleImageChange} />
+            <label className="image-select" for="image">Choose an Image</label>
+            <input type="file" id="image" name="image" onChange={handleImageChange} />
           </span>
         </div>
         {image && (
           <div className="row">
-            <span className="action">2. Set a horrible task</span>
             <span className="action">
-              <CheckBox onChange={toggleComplete} />
               <input 
                 type="text" 
                 name="text" 
-                placeholder="a horrible no good task" 
+                placeholder="enter your horrible task" 
                 onChange={handleChange} 
               />
             </span>
@@ -92,22 +95,33 @@ function App() {
             height: imageSize.h,
           }}>
             <img className="bg" src={image} alt="todo" />
-            <img className="todo" src={todoBg} />
-            <span className="todo-text"
-              style={{
-                textDecoration: complete ? 'line-through' : 'none'
-              }}
-            >
-              {text}
-            </span>
+            <div className="todo">
+              <img className="todo-img" src={todoBg} />
+              <span className="todo-text"
+                style={{
+                  textDecoration: complete ? 'line-through' : 'none'
+                }}
+              >
+                {text}
+              </span>
+            </div>
           </div>
         </div>
         
       )}
 
       {image && (
-        <button onClick={download}>Download</button>
+        <div>
+          <div className="complete">
+            <CheckBox onChange={toggleComplete} />
+            <p>Complete your no-good task?</p>
+          </div>
+          <button onClick={download}>honk</button>
+        </div>
+        
       )}
+
+      
       
     </div>
   );
