66bfef613fa3e79b2a5edd53bd2637802cde7705
Chris Grice <chris@chrisgrice.com>
2019-10-02 17:59:31 +0100

    Trying constructor credentials

--- src/functions/image.js
+++ src/functions/image.js
@@ -2,11 +2,12 @@ const AWS = require('aws-sdk')
 const path = require('path')
 
 exports.handler = function(event, context, callback) {
-    process.env.AWS_ACCESS_KEY_ID = process.env.ENV_AWS_ACCESS_KEY_ID
-    process.env.AWS_SECRET_ACCESS_KEY = process.env.ENV_AWS_SECRET_ACCESS_KEY
-    process.env.AWS_REGION = process.env.ENV_AWS_REGION
-    process.env.AWS_SESSION_TOKEN = ''
-    const s3 = new AWS.S3()
+    
+    const s3 = new AWS.S3({
+        region : process.env.ENV_AWS_REGION,
+        accessKeyId : process.env.ENV_AWS_ACCESS_KEY_ID,
+        secretAccessKey : process.env.ENV_AWS_SECRET_ACCESS_KEY,
+    })
 
     const key = path.basename(event.path)
     
